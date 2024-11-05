0e8dc8eabd8d90d86b752a4be35b6feee947acb4
Tom McClain <90461522+jctmcclain@users.noreply.github.com>
2024-11-05 09:32:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-
+<html>
+<body>
+<?php
+echo('<h1>Welcome to our cool site</h1>');
+echo('<hr>');
+$speaktome = "IT 105 programming";
+echo('<h3>' + $speaktome + '</h3>'); 
+?>
+<p>
+<b>PHP</b>
+</p>
+  <p>
+    PHP: Hypertext Preprocessor<br>
+    Developed 1993 - Released 1995 
+  </p>
+</body>
+</html>
